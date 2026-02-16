71d31263f8fede67430ff588c28cdf8bfb06c845
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-16 21:36:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
-echo "desimal : \n";
-var_dump (false);
+echo "desimal : ";
+var_dump (123);
+echo "string : ";
+var_dump ("hello world");
+echo "float : ";
+var_dump (2.5);
+echo "hexadesimal : ";
+var_dump (0x1A);
+echo "binary : ";
+var_dump (0b111111111111);
+echo "undescore number : ";
+var_dump (1_234_5);
