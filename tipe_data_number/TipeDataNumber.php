4349e8a09c61f54d3f6ba4d71c345a754bf03886
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-19 21:30:13 +0700

    woe kont** internet kos gua kek kont**
    ꦏꦺꦴꦤ꧀ꦠꦺꦴꦭ꧀ ꦤ꧀ꦒꦺꦤ꧀ꦠꦺꦴꦢ꧀ ꦲꦺꦩꦤ꧀ꦒ꧀ ꦗꦤ꧀ꦕꦺꦴꦏ꧀

---
+++ tipe_data_number/TipeDataNumber.php
@@ -0,0 +1,14 @@
+<?php
+
+function TipeDataNumber() {
+echo "Decimal : ";
+var_dump (1234);
+echo "Octal : ";
+var_dump (0123);
+echo "Hexadesimal : ";
+var_dump (0x1A);
+echo "Binary : ";
+var_dump (0b11111111);
+echo "Undescore di Number : ";
+var_dump (1_234_567);
+} 
