2a363fc6ffa8cdab32a2c061feadea4349ec25c4
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-20 11:45:09 +0700

    oke

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -10,22 +10,53 @@
 // Penulisan Sintaks PHP
 // 1. PHP di dalam HTML
 // 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Nadif Zidan Tazaka";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// . 
+// $nama_depan = "Nadif";
+// $nama_belakang = "Zidan T";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 2;
+// $x -= 5;
+// echo $x;
+// $nama = "Nadif";
+// $nama .= " ";
+// $nama .= "Zidan T";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==, 
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+$x = 30;
+var_dump ($x < 20 && $x % 2 == 0);
+
+
+
+
+
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Belajar PHP Dasar</title>
-</head>
-<body>
-  <h1>
-    Halo, Selamat Datang Nadif 
-    <?php 
-    echo "Nadif Zidan T";
-    ?>
-  </h1>  
-
-</body>
-</html>
