a0c068cf64b92bb93d4d941eab96186418837c72
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-20 11:14:56 +0700

    woi

---
+++ pertemuan2/index.php
@@ -0,0 +1,31 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Syntax PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar PHP Dasar</title>
+</head>
+<body>
+  <h1>
+    Halo, Selamat Datang Nadif 
+    <?php 
+    echo "Nadif Zidan T";
+    ?>
+  </h1>  
+
+</body>
+</html>
