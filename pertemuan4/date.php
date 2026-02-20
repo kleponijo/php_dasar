3ce9c8c6c5f08fe8d20778c8d7bd964f63c676a7
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-21 04:01:57 +0700

    beuhh

---
+++ pertemuan4/date.php
@@ -0,0 +1,12 @@
+<?php 
+// Date
+// Untuk menampilkan tanggal dengan format tertentu
+// echo date("l, d-M-Y");
+
+// Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 Januari 1970
+// echo time();
+echo date("l", time() + 172800);
+
+?>
