63a3ef38daf7dbc5b8b2a1f2da4816d1f528e28e
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-21 07:18:16 +0700

    bouh

--- pertemuan4/date.php
+++ pertemuan4/date.php
@@ -1,4 +1,6 @@
 <?php 
+// setting default timezone di php menggunakan date_default_timezone_set("");
+date_default_timezone_set("Asia/Jakarta");
 // Date
 // Untuk menampilkan tanggal dengan format tertentu
 // echo date("l, d-M-Y");
@@ -7,6 +9,19 @@
 // UNIX Timestamp / EPOCH time
 // detik yang sudah berlalu sejak 1 Januari 1970
 // echo time();
-echo date("l", time() + 172800);
+// echo date("l", time() - 60*60*24*100);
+
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0);
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0,10,4,2004));
+
+// strtotime
+echo date("l", strtotime("4 october 2004"));
+
+
+
+
 
 ?>
